e52195305b78e4f18f1de587f44449fc691b0c77
Tomás Coias <tomascoias04@gmail.com>
2026-05-11 19:31:46 +0100

    Update project-cli.c
    Adaptei para o servidor. Apenas meti codigo que estava no main nas funcoes e adicionei as threads que e preciso

--- projeto-cli.c
+++ projeto-cli.c
@@ -1,4 +1,4 @@
-// projeto-cli2.0.c
+// projeto-cli3.0.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -8,17 +8,57 @@
 #include <sys/un.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <pthread.h>
 #define exit_on_error(s,m) if ( s < 0 ) { perror(m); exit(1); }
 // Estrutura de Mensagem
 typedef struct {
 char nome[50];
 char mensagem[500];
 } ChatMsg;
+
+// Thread enviar mensagens
+void *enviar_mensagens(void *arg){
+  int socket_client = *(int *)arg;
+  ChatMsg m;
+  printf("Nome: ");
+  fgets(m.nome, 50, stdin);
+  m.nome[strcspn(m.nome, "\n")] = 0; //Remover \n
+
+  while(1){
+    printf("Mensagem:");
+    fgets(m.mensagem, 500, stdin);
+    int n = send(socket_client, &m, sizeof(m), 0);
+    if (n <= 0){
+      print("Erro ao enviar mensagem.\n");
+      break;
+    }
+  }
+  return NULL;
+}
+
+//Thread receber mensagens
+void *receber_mensagens(void *args){
+  int socket_client = *(int *)arg;
+  ChatMsg m;
+
+  while(1){
+    int n = recv(socket_client, &m, sizeof(m), 0);
+    if(n <= 0){
+      printf("Servidor desconectado. \n");
+      break;
+    }
+
+    printf("\n[%s]: %s", m.nome, m.mensagem);
+    printf("Mensagem: ");
+    fflush(stdout);
+  }
+  return NULL;
+}
 main() {
 // Socket Client
 int s = socket ( PF_INET, SOCK_STREAM, 0 );
 exit_on_error ( s, "socket");
-// Endereco do servido
+// Endereco do servidor
 struct sockaddr_in s_addr;
 s_addr.sin_family = AF_INET;
 s_addr.sin_addr.s_addr = inet_addr ( "127.0.0.1" );
@@ -28,22 +68,30 @@ int status;
 status=connect( s, (struct sockaddr*)&s_addr, sizeof(s_addr) );
 exit_on_error ( status, "connect");
 printf("Ligado ao servidor!\n");
-// Criar a estrutura
-while(1){
-ChatMsg m;
-printf("Nome:");
-fgets(m.nome, 50, stdin);
-m.nome[strcspn(m.nome, "\n")] = 0; //Remover \n
-printf("Mensagem:");
-fgets(m.mensagem, 500, stdin);
-// Enviar a struct
-int n = send(s, &m, sizeof(m), 0);
-exit_on_error(n, "send");
-// Receber a resposta
-char resposta[100];
-n = recv(s, resposta, sizeof(resposta), 0);
-exit_on_error(n, "recv");
-printf("Servidor: %s\n", resposta);
-}
+
+// Threads
+pthread_t thread_enviar;
+pthread_t thread_receber;
+// Thread enviar
+pthread_create(
+  &thread_enviar,
+  NULL,
+  enviar_mensagens,
+  (void *)&s
+);
+
+// Thread receber
+pthread_create(
+  &thread_receber,
+  NULL,
+  receber_mensagens,
+  (void *)&s
+);
+
+// Esperar threads
+pthread_join(thread_enviar, NULL);
+pthread_join(thread_receber, NULL);
+
 close(s);
+return 0;
 }
